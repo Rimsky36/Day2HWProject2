80292ebc8884a5f433386fadd70d71d088544db3
Rimsky <Rimsky@192.168.1.145>
2023-04-09 13:27:58 -0700

    adding multiply() method

--- src/com/HWProject2/DemoProyect2.java
+++ src/com/HWProject2/DemoProyect2.java
@@ -5,7 +5,13 @@ public class DemoProyect2 {
 	public static void main(String[] args) {
 		
 		System.out.println("New test project2");
+		
+		System.out.println("New test project2");
 
 	}
+public void multiply(int a, int b) {
+	
+	System.out.println(a*b);
 
+}
 }
